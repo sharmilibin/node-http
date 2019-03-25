c3a6156f1071625f1364b924af3570a806975832
Sharmi Libin <sharmi.m.rose@gmail.com>
2019-03-24 20:09:13 -0600

    first commit

---
+++ Index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const host = 'localhost';
+const port = '3000';
+
+const server = http.createServer((req,res) => {
+   console.log(req.headers) ;
+   res.statusCode = '200';
+   res.setHeader('content-Type','text/html' );
+   res.end('<html><head></head><body><h1>This is my First server</h1></body></html>');
+});
+
+server.listen(port, host, () => {
+    console.log(`server running @ http://${host}:${port}`);
+});
+
